92f46e2da3ab63552fa2509b5971240c7e3a6a6b
Anthony <112771704+Bo0py@users.noreply.github.com>
2023-02-15 14:26:29 +0000

    Player Changes. Contains movement for WASD controls

--- player.js
+++ player.js
@@ -4,13 +4,14 @@ var gravPull = 1;
 var jumpOn;
 var falling = false;
 var jumping = false;
+var goingLeft = false;
+var goingRight = false; var L; var R;
 
-var groundBorder = 80; //Ground Border
+var groundBorder = 83; //Ground Border
 
 function refresh() {
   player.icon.style.top = player.y + "%"
   player.icon.style.left = player.x + "%"
-  console.log("X: "+player.icon.style.left+"/ Y: "+ player.icon.style.top);
   if (player.y < groundBorder && falling == false && jumping == false) {
     falling = true;
     gravityOn = setInterval(gravity, 20);
@@ -19,18 +20,22 @@ function refresh() {
 
 document.addEventListener('keyup', event => {
   if (event.code === 'Space') {
-    if (jumping == false && falling == false) {
-      jumping = true;
-      gravPull = 1;
-      jumpOn = setInterval(jump, 10);
-      setTimeout(function(){
-        clearInterval(jumpOn); 
-        jumping = false;
-        refresh();
-      }, 1000);
-    }
+    toJump();
   }
 })
+
+function toJump() {
+  if (jumping == false && falling == false) {
+    jumping = true;
+    gravPull = 1;
+    jumpOn = setInterval(jump, 10);
+    setTimeout(function() {
+      clearInterval(jumpOn);
+      jumping = false;
+      refresh();
+    }, 1000);
+  }
+}
 
 function gravity() {
   if (player.y < groundBorder) {
@@ -46,7 +51,59 @@ function gravity() {
 }
 
 function jump() {
+  console.log("Jumping")
   player.y -= gravPull;
   gravPull -= 0.01
   refresh()
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'w')
+    toJump();
+})
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'a')
+    if (goingLeft == false) {
+      goingLeft = true;
+      L = setInterval(moveLeft, 10);
+      setTimeout(function() {
+        goingLeft = false;
+        clearInterval(L);
+      }, 500);
+    }
+})
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'd')
+    if (goingRight == false) {
+      goingRight = true;
+      R = setInterval(moveRight, 10);
+      setTimeout(function() {
+        goingRight = false;
+        clearInterval(R);
+      }, 500);
+    }
+})
+
+function moveLeft() {
+  if (player.x > 0) {
+    console.log("Going Left")
+    player.x -= 0.5;
+    refresh()
+  }
+  else {
+    console.log("Hitting Border");
+  }
+}
+
+function moveRight() {
+  if (player.x < 90) {
+    console.log("Going Right")
+    player.x += 0.5;
+    refresh();
+  } 
+  else {
+    console.log("Hitting Border");
+  }
 }
